480cb24c4a926e06e904c1ab925cf59a68cbf981
yashwalhekar <yashwalhekar371@gmail.com>
2026-01-19 11:14:03 +0530

    remove cookies logic

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://billing-web-app-coral.vercel.app//api",
-  withCredentials: true,
+  baseURL: "https://billing-web-app-coral.vercel.app/api",
 });
 
+// ⬇️ Add this to send token with ALL protected requests
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
 export default API;
