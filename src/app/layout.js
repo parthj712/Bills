4eb48dc6c3da76909bf31c40439a64f7070a5ed9
parth <parth.j@coditude.com>
2026-02-05 11:13:29 +0530

    add login toast and logout toast

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,10 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./Providers";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "@/Componenets/ToastConstant/toast.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +27,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        <Providers>{children}
+           <ToastContainer />
+        </Providers>
       </body>
     </html>
   );
